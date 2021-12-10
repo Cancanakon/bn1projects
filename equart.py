8506671f5e154db50fda7e89e70bcc1f26dd3b02
Cancanakon <95315162+Cancanakon@users.noreply.github.com>
2021-12-10 21:51:57 +0300

    Initial commit

---
+++ equart.py
@@ -0,0 +1,94 @@
+from PyQt5.QtWidgets import *
+import requests
+from designer import Ui_frmMain
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon ,QKeySequence
+from bs4 import BeautifulSoup
+import json
+
+class deprem(QMainWindow):
+
+ def __init__(self):
+    super().__init__()
+
+    self.ui = Ui_frmMain()
+    self.ui.setupUi(self)
+
+    self.ui.btnExt.setToolTip("Ctrl+Q ya bas")
+    self.ui.btnUpdate.setToolTip("Ctrl+U ya bas")
+    self.ui.btnAll.setToolTip("Ctrl+A ya bas")
+    self.ui.btnDanger.setToolTip("Ctrl+D ye bas")
+
+
+
+    self.ui.btnExt.clicked.connect(self.btnExtClickedSlot)
+    self.ui.lndtSehir.returnPressed.connect(self.procces1)
+    self.ui.lndtDeger.returnPressed.connect(self.procces)
+
+ def procces1(self):
+     r = requests.get("https://turkiyedepremapi.herokuapp.com")
+     received_signalCity = self.ui.lndtSehir.text()
+     payload = {}
+     headers = {}
+     responseCity = requests.request("GET", url="https://turkiyedepremapi.herokuapp.com/api?sehir=(" + received_signalCity + ")",
+                                 headers=headers, data=payload)
+     resultCity = json.loads(responseCity.text)
+     print("İstenilen Şehir: ", received_signalCity)
+     print(resultCity)
+
+
+ def procces(self):
+      r = requests.get("https://turkiyedepremapi.herokuapp.com")
+
+      received_signal = self.ui.lndtDeger.text()
+
+      payload = {}
+      headers = {}
+      responseValue = requests.request("GET",
+                                  url="https://turkiyedepremapi.herokuapp.com/api?min="+received_signal+"&max="+received_signal,
+                                  headers=headers, data=payload)
+      resultValue = json.loads(responseValue.text)
+      print("Deprem Büyüklüğü: ", received_signal)
+      print(resultValue)
+
+
+      # print("Alınan Yazı" , received_signal)
+
+
+ def btnExtClickedSlot(self):
+     print("Çıkılıyor")
+     quit()
+
+
+
+
+
+
+
+
+# def sehir():
+#
+#       sehir = input("Şehir Yazınız:  ")
+#       r = requests.get("https://turkiyedepremapi.herokuapp.com")
+#       print("Sunucu Yanıtı:  " , r.status_code)
+#       if r.status_code == 200:
+#           print("Durum:   Bağlantı Başarılı")
+#
+#
+#
+#       payload = {}
+#       headers = {}
+#       response = requests.request("GET", url="https://turkiyedepremapi.herokuapp.com/api?sehir=("+sehir+")", headers=headers, data=payload)
+#       result = json.loads(response.text)
+#       print(result)
+#
+#
+# print(sehir())
+#
+
+
+
+uygulama = QApplication([])
+pencere = deprem()
+pencere.show()
+uygulama.exec()
---
+++ main2.py
@@ -0,0 +1,45 @@
+
+# from PyQt5.QtWidgets import *
+# from designer import Ui_Form
+#
+# class Ana(QMainWindow):
+#
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.ui = Ui_Form()
+#         self.ui.setupUi(self)
+#     def slotDeger(self,index):
+#         self.ui.lblDeger.setText("Değer=="+str (index))
+#
+# uygulama = QApplication([])
+# pencere = Ana()
+# pencere.show()
+# uygulama.exec()
+#------------------------------
+from PyQt5.QtWidgets import *
+import requests
+from designer import Ui_frmMain
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon ,QKeySequence
+from bs4 import BeautifulSoup
+
+# class deprem(QMainWindow):
+#
+#     def __init__(self):
+#
+#         super().__init__()
+#
+#         self.ui = Ui_frmMain()
+#         self.ui.setupUi(self)
+#
+#
+#
+#
+#
+# uygulama = QApplication([])
+# pencere = deprem()
+# pencere.show()
+# uygulama.exec()
+
+
